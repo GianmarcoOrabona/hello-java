50c65468219b9c89f2e57d48f4a2ce44df2ad4f5
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-28 13:35:49 +0100

    final commit

---
+++ src/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,8 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        // Stampo un saluto
+        System.out.println("Hello Java!");
+    }
+}
